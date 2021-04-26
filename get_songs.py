584877f618db275e9a83a869c628dc90df6920bd
pankace <44595774+pankace@users.noreply.github.com>
2021-04-26 15:16:42 +0700

    test??

--- get_songs.py
+++ get_songs.py
@@ -9,19 +9,19 @@ def main():
 #Select what you want to study by un-commenting and changing the file name the in "get_audio_features.py"
 #defult is set to top50 weekly 
 #Don't forget to uncomment threads 
-    #a_thread = threading.Thread(target = api.top200Daily, kwargs = 
-    #{"output_file": "top_200_daily.csv", "output_db": connector, "webhook": hooks, "start": "2020-01-03", "end":"2020-01-12", "region": ["global", "us"]}) #Change variables as needed
-    #b_thread = threading.Thread(target = api.top200Weekly, kwargs = 
-    #{"output_file": "top_200_weekly.csv", "output_db": connector, "webhook": hooks, "start": "2020-01-03", "end":"2020-01-12", "region": ["global", "us"]}) #
-    #c_thread = threading.Thread(target = api.viral50Daily, kwargs = 
-    #{"output_file": "viral_50_daily.csv", "output_db": connector, "webhook": hooks, "start": "2020-01-03", "end":"2020-01-12", "region": ["global", "us"]}) #
+    a_thread = threading.Thread(target = api.top200Daily, kwargs = 
+    {"output_file": "top_200_daily.csv", "output_db": connector, "webhook": hooks, "start": "2020-01-03", "end":"2020-01-12", "region": ["global", "us"]}) #Change variables as needed
+    b_thread = threading.Thread(target = api.top200Weekly, kwargs = 
+    {"output_file": "top_200_weekly.csv", "output_db": connector, "webhook": hooks, "start": "2020-01-03", "end":"2020-01-12", "region": ["global", "us"]}) #
+    c_thread = threading.Thread(target = api.viral50Daily, kwargs = 
+    {"output_file": "viral_50_daily.csv", "output_db": connector, "webhook": hooks, "start": "2020-01-03", "end":"2020-01-12", "region": ["global", "us"]}) #
     d_thread = threading.Thread(target = api.viral50Weekly, kwargs = 
     {"output_file": "viral_50_weekly.csv", "output_db": connector, "webhook": hooks, "start": "2020-01-02", "end":"2020-01-12", "region": ["global", "us"]}) #
 
 #when in use uncomment these too 
-    #a_thread.start() 
-    #b_thread.start()
-    #c_thread.start()
+    a_thread.start() 
+    b_thread.start()
+    c_thread.start()
     d_thread.start()
 
 if __name__ == "__main__":
